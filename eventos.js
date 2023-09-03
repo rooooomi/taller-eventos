6bc1b83e28f0e2ab233e34e56477a1441c6e0c6f
romi <roalvesvalerio@gmail.com>
2023-09-03 18:52:50 -0300

    alerta div

---
+++ eventos.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  const divi = document.getElementById('divboton');
+  divi.addEventListener('click', function(){
+    alert ("¡Hola! Soy el div."); 
+  })
+})
+
+
